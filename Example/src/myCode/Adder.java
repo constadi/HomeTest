6cf35b42ba762fa03e01746799cca537313a8fab
constadi <diana.constable@hotmail.com>
2014-01-20 22:39:59 +0000

    Update Adder.java

--- Example/src/myCode/Adder.java
+++ Example/src/myCode/Adder.java
@@ -8,7 +8,7 @@ public class Adder implements Operation {
 	@Override
 	public int add(int input1, int input2) {
 		// TODO Auto-generated method stub
-		return input1 + input2;
+		return input1 + input2; // doing a change for push and pull
 	}
 
 }
