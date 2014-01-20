1fe778eb271c69cf23efb49f6f24c076ea8a7036
Diana Constable <ladydianauk@gmail.com>
2014-01-20 22:35:17 +0000

    commit

---
+++ Example/src/myCode/Adder.java
@@ -0,0 +1,14 @@
+package myCode;
+
+public class Adder implements Operation {
+
+	/* (non-Javadoc)
+	 * @see myCode.Operation#add(int, int)
+	 */
+	@Override
+	public int add(int input1, int input2) {
+		// TODO Auto-generated method stub
+		return input1 + input2;
+	}
+
+}
